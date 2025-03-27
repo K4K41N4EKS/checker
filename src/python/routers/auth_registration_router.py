98cd941c24d643b05653a0e21ae51c2bc237a6e6
Konstantin <kostalunavgogle@gmail.com>
2025-03-27 15:16:16 +0700

    added the defence system of internal routes

--- src/python/routers/auth_registration_router.py
+++ src/python/routers/auth_registration_router.py
@@ -3,11 +3,20 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from src.python.database.database import get_db
 from src.python.services.user_sync_services import sync_user
+import os
 
 router = APIRouter()
 
-@router.post("/create_user")
+INTERNAL_SECRET = os.getenv("INTERNAL_SERVICE_SECRET")
+if not INTERNAL_SECRET:
+    raise RuntimeError("INTERNAL_SERVICE_SECRET is not set!")
+
+@router.post("/create_user", include_in_schema=False)
 def create_user(request: Request, db: Session = Depends(get_db)):
+    incoming_secret = request.headers.get("X-Internal-Secret")
+    if incoming_secret != INTERNAL_SECRET:
+        raise HTTPException(status_code=403, detail="Forbidden")
+
     user_id = request.headers.get("user_id")
     username = request.headers.get("username")
 
