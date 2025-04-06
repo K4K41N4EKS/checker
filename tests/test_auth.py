cc8d8bef6ada23311657ba90c5b033e032febca7
kir4h <kir4h@K4KPCU.myguest.virtualbox.org>
2025-04-06 23:37:45 +0700

    Auth Autotests

---
+++ tests/test_auth.py
@@ -0,0 +1,64 @@
+def test_successful_login(test_client, registered_user, logged_user):
+    assert "access_token" in logged_user, \
+        f"Expected access_token not found. Response: {logged_user}"
+
+    assert "refresh_token" in logged_user, \
+        f"Expected refresh_token not found. Response: {logged_user}"
+
+
+def test_login_with_wrong_password(test_client, registered_user, auth_url, test_user):
+    response = test_client.get(
+        f"{auth_url}/login",
+        headers={
+            "username": test_user["username"], 
+            "passwd": "12354"}
+    )
+    
+    assert response.status_code == 401, \
+        f"Expected 401, got {response.status_code}. Response: {response.json()}"
+    
+    status = response.json()["status"]
+    assert status == "error", \
+        f"Expected status:error, got {status}. Response: {response.json()}"
+
+
+def test_access(test_client, registered_user, logged_user, app_url):
+    refresh_token = logged_user["refresh_token"]
+    response = test_client.get(
+        f"{app_url}/templates",
+        headers={"Authorization": f"Bearer {refresh_token}"}
+    )
+
+    assert response.status_code == 200, \
+        f"Expected 200, got {response.status_code}. Response: {response.json()}"
+    
+
+def test_refresh(test_client, registered_user, logged_user, auth_url, app_url):
+    response = test_client.post(
+        f"{auth_url}/updateaccesst",
+        headers={
+            "refresh-token" : logged_user["refresh_token"]}
+    )
+    
+    assert response.status_code == 200, \
+        f"Expected 200, got {response.status_code}. Response: {response.json()}"
+    
+    refresh_token = logged_user["refresh_token"]
+    response = test_client.get(
+        f"{app_url}/templates",
+        headers={"Authorization": f"Bearer {refresh_token}"}
+    )
+
+    assert response.status_code == 200, \
+        f"Expected 200, got {response.status_code}. Response: {response.json()}"
+    
+
+def test_logout(test_client, registered_user, logged_user, auth_url, test_user):
+    response = test_client.post(
+        f"{auth_url}/logout",
+        headers={
+            "refresh-token" : logged_user["refresh_token"]}
+    )
+    
+    assert response.status_code == 200, \
+        f"Expected 200, got {response.status_code}. Response: {response.json()}"
---
+++ tests/test_registration_request.py
@@ -0,0 +1,24 @@
+def test_registration(test_client, registered_user):
+    assert registered_user.status_code == 200, \
+        f"Expected 200, got {registered_user.status_code}. Response: {registered_user.json()}"
+    
+    # service_db_user = service_db.query(User).order_by(User.id.desc()).first()
+    # app_db_user = app_db.query(User).order_by(User.id.desc()).first()
+    
+    # assert app_db_user.id is not None, "User ID missing in app DB"
+    # assert app_db_user.username == "test", f"Expected username 'test', got {app_db_user.username}"
+    
+    # # Repeat for service_db (ensure it's also a Session)
+    # assert service_db_user.user_id == app_db_user.id, "ID mismatch between DBs"
+
+def test_registration_second_attempt(test_client, auth_url, test_user):
+    response = test_client.post(
+        f"{auth_url}/registration",
+        headers={
+            "username": test_user["username"], 
+            "passwd": test_user["password"]}
+    )
+    
+    assert response.status_code == 409, \
+        f"Expected 409, got {response.status_code}. Response: {response.json()}"
+    
