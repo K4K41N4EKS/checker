91a09db271879d8c174aa53b5df08b33673e879a
dipsomask <dipsomask@gmail.com>
2025-04-12 10:28:51 +0700

    изменил маршруты для login и registration

--- src/js/server.js
+++ src/js/server.js
@@ -8,6 +8,13 @@ app.use('/css', express.static(path.join(__dirname, '../../views/css')));
 app.use('/js', express.static(path.join(__dirname, '.')));
 
 
+app.get('/checker/registration', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../views/html/registration.html'));
+});
+app.get('/checker/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../views/html/login.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
